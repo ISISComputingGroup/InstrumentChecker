9ae77e7828d1e24ed643bc63b7d40a9109240454
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-09-26 21:02:14 +0100

    reduce batch file usage

--- run_tests.bat
+++ run_tests.bat
@@ -6,27 +6,14 @@ set configs_dir=%~dp0\configs
 set gui_dir=%~dp0\gui
 set reports_dir=%~dp0\test-reports
 
-REM Clone/pull necessary repositories
-if exist %configs_dir% (
-	cd %configs_dir%
-	git reset HEAD --hard
-	git clean -fdx
-    git pull
-	cd %~dp0
-) else (
-    git clone http://spudulike@control-svcs.isis.cclrc.ac.uk/gitroot/instconfigs/inst.git %configs_dir%
+REM Clone necessary repositories
+if not exist "%configs_dir%" (
+    git clone http://spudulike@control-svcs.isis.cclrc.ac.uk/gitroot/instconfigs/inst.git "%configs_dir%"
 )
 
-REM Clone/pull necessary repositories
-if exist %gui_dir% (
-	cd %gui_dir%
-	git reset HEAD --hard
-	git clean -fdx
-    git pull
-	cd %~dp0
-) else (
-    git clone https://github.com/ISISComputingGroup/ibex_gui.git %gui_dir%
+if not exist "%gui_dir%" (
+    git clone https://github.com/ISISComputingGroup/ibex_gui.git "%gui_dir%"
 )
 
-python run_tests.py --configs_repo_path %configs_dir% --gui_repo_path %gui_dir% --reports_path %reports_dir%
+python run_tests.py --configs_repo_path "%configs_dir%" --gui_repo_path "%gui_dir%" --reports_path "%reports_dir%"
 if %errorlevel% neq 0 exit /b %errorlevel%
