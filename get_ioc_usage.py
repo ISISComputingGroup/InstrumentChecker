3f853eba3c91e960cd43642f59d3e6012279fd09
John-Holt-Tessella <John.Holt@tessella.com>
2018-03-12 16:18:04 +0000

    Tidy

--- get_ioc_usage.py
+++ get_ioc_usage.py
@@ -1,75 +1,11 @@
 import os
-import sys
-import unittest
-from xmlrunner import XMLTestRunner
 import argparse
 
-from tests.configuration_tests import ConfigurationsTests, ConfigurationsSingleTests
-from tests.component_tests import ComponentsTests, ComponentsSingleTests
-from tests.globals_tests import GlobalsTests
-from tests.scripting_directory_tests import ScriptingDirectoryTests
-from tests.synoptic_tests import SynopticTests
-from tests.version_tests import VersionTests
+from run_tests import setup_instrument_tests
 from tests.settings import Settings
 
 from util.channel_access import ChannelAccessUtils
 from util.configurations import ConfigurationUtils, ComponentUtils
-from util.git_wrapper import GitUtils
-from util.synoptic import SynopticUtils
-
-
-def run_instrument_tests(inst_name, reports_path):
-    """
-    Runs the test suite
-    :param inst_name: The name of the instrument to run tests on,
-                    used to sort the test reports folder into instrument-specific reports
-    :param reports_path: The path to store test reports
-    :return: True if the tests passed, false otherwise
-    """
-    suite = unittest.TestSuite()
-    loader = unittest.TestLoader()
-
-    for case in [ScriptingDirectoryTests, GlobalsTests, VersionTests, ConfigurationsSingleTests, ComponentsSingleTests]:
-        suite.addTests(loader.loadTestsFromTestCase(case))
-
-    # Add configs test suite a dynamic number of times with an argument of the config name.
-    # unittest's test loader is unable to take arguments to test classes by default so have
-    # to use the getTestCaseNames() syntax and explicitly add the argument ourselves.
-    for config in ConfigurationUtils(Settings.config_repo_path).get_configurations_as_list():
-        suite.addTests([ConfigurationsTests(test, config) for test in loader.getTestCaseNames(ConfigurationsTests)])
-
-    for component in ComponentUtils(Settings.config_repo_path).get_configurations_as_list():
-        suite.addTests([ComponentsTests(test, component) for test in loader.getTestCaseNames(ComponentsTests)])
-
-    for synoptic in SynopticUtils(Settings.config_repo_path).get_synoptics_filenames():
-        suite.addTests([SynopticTests(test, synoptic) for test in loader.getTestCaseNames(SynopticTests)])
-
-    runner = XMLTestRunner(output=str(os.path.join(reports_path, inst_name)), stream=sys.stdout)
-    return runner.run(suite).wasSuccessful()
-
-
-def setup_instrument_tests(instrument):
-    """
-    Sets up the settings class and configurations repository to point at the given instrument.
-
-    :param instrument: A dictionary representing the properties of an instrument as per the CS:INSTLIST PV.
-    :return: True if successful, False otherwise.
-    """
-    name, hostname, pv_prefix = instrument['name'], instrument['hostName'], instrument['pvPrefix']
-    Settings.set_instrument(name, hostname, pv_prefix)
-
-    print("\n\nChecking out git repository for {} ({})...".format(name, hostname))
-    return GitUtils(Settings.config_repo_path).update_branch(hostname)
-
-
-def run_self_tests(reports_path):
-    """
-    Runs our own unit tests.
-    :return: True if all tests passed, False otherwise
-    """
-    print("Running self-tests...")
-    suite = unittest.TestLoader().discover(os.path.join("util", "test_utils"))
-    return XMLTestRunner(output=str(reports_path), stream=sys.stdout).run(suite).wasSuccessful()
 
 
 def calc_iocs_on_intruments(instruments):
