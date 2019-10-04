bfc4b67dd73d7596360a7372b7b8649e525a905e
Alexandru Dascalu <965337@swansea.ac.uk>
2019-10-04 11:15:54 +0100

    added common xml string which is reused by multiple tests

--- util/test_utils/test_configurations.py
+++ util/test_utils/test_configurations.py
@@ -1,123 +1,116 @@
 from util.configurations import ConfigurationUtils
 import unittest
 
+generic_component_xml = """<?xml version="1.0" ?>
+             <components xmlns="http://epics.isis.rl.ac.uk/schema/components/1.0"
+             xmlns:comp="http://epics.isis.rl.ac.uk/schema/components/1.0"
+             xmlns:xi="http://www.w3.org/2001/XInclude">
+                 <component name="COMPONENT_1"/>
+                 {}
+             </components>
+      """
+
+generic_block_xml = """<?xml version="1.0" ?>
+                    <blocks xmlns="http://epics.isis.rl.ac.uk/schema/blocks/1.0" 
+                    xmlns:blk="http://epics.isis.rl.ac.uk/schema/blocks/1.0" xmlns:xi="http://www.w3.org/2001/XInclude">
+                        {}
+                        {}
+                        {}
+                    </blocks>"""
+
 
 class ConfigurationTests(unittest.TestCase):
+
     def setUp(self):
         self.config_utils = ConfigurationUtils("")
 
     def test_GIVEN_component_xml_WHEN_parsed_THEN_can_extract_a_single_component(self):
-        xml = """<?xml version="1.0" ?>
-                     <components xmlns="http://epics.isis.rl.ac.uk/schema/components/1.0"
-                     xmlns:comp="http://epics.isis.rl.ac.uk/schema/components/1.0"
-                     xmlns:xi="http://www.w3.org/2001/XInclude">
-                         <component name="COMPONENT_1"/>
-                     </components>
-              """
+        xml = generic_component_xml.format("")
 
         self.assertListEqual(self.config_utils.get_active_components_from_xml(xml), ["COMPONENT_1"])
 
     def test_GIVEN_component_xml_WHEN_parsed_THEN_can_extract_multiple_components(self):
-        xml = """<?xml version="1.0" ?>
-                     <components xmlns="http://epics.isis.rl.ac.uk/schema/components/1.0"
-                     xmlns:comp="http://epics.isis.rl.ac.uk/schema/components/1.0"
-                     xmlns:xi="http://www.w3.org/2001/XInclude">
-                         <component name="COMPONENT_1"/>
-                         <component name="COMPONENT_2"/>
-                     </components>
-              """
+        xml = generic_component_xml.format('<component name="COMPONENT_2"/>')
 
         self.assertListEqual(self.config_utils.get_active_components_from_xml(xml), ["COMPONENT_1", "COMPONENT_2"])
 
     def test_GIVEN_block_xml_WHEN_parsed_and_no_blocks_THEN_return_no_data(self):
-        xml = """<?xml version="1.0" ?>
-                    <blocks xmlns="http://epics.isis.rl.ac.uk/schema/blocks/1.0" 
-                    xmlns:blk="http://epics.isis.rl.ac.uk/schema/blocks/1.0" xmlns:xi="http://www.w3.org/2001/XInclude">
-                    </blocks>"""
+        xml = generic_block_xml.format('', '', '')
 
         self.assertListEqual(self.config_utils.get_block_pvs_from_xml("", xml), [])
 
     def test_GIVEN_block_xml_WHEN_parsed_THEN_can_extract_one_global_pv(self):
-        xml = """<?xml version="1.0" ?>
-                    <blocks xmlns="http://epics.isis.rl.ac.uk/schema/blocks/1.0" 
-                    xmlns:blk="http://epics.isis.rl.ac.uk/schema/blocks/1.0" xmlns:xi="http://www.w3.org/2001/XInclude">
-                        <block>
-                            <name>S2VG</name>
-                            <read_pv>MOT:JAWS2:VGAP</read_pv>
-                            <local>False</local>
-                            <visible>True</visible>
-                            <rc_enabled>False</rc_enabled>
-                            <rc_lowlimit>0.0</rc_lowlimit>
-                            <rc_highlimit>0.0</rc_highlimit>
-                            <log_periodic>True</log_periodic>
-                            <log_rate>30</log_rate>
-                            <log_deadband>0.0</log_deadband>
-                        </block>
-                    </blocks>"""
+        xml = generic_block_xml.format(
+                                """<block>
+                                    <name>S2VG</name>
+                                    <read_pv>MOT:JAWS2:VGAP</read_pv>
+                                    <local>False</local>
+                                    <visible>True</visible>
+                                    <rc_enabled>False</rc_enabled>
+                                    <rc_lowlimit>0.0</rc_lowlimit>
+                                    <rc_highlimit>0.0</rc_highlimit>
+                                    <log_periodic>True</log_periodic>
+                                    <log_rate>30</log_rate>
+                                    <log_deadband>0.0</log_deadband>
+                                </block>""", "", "")
 
         self.assertListEqual(self.config_utils.get_block_pvs_from_xml("IN:DEMO", xml), ["MOT:JAWS2:VGAP"])
 
     def test_GIVEN_block_xml_WHEN_parsed_THEN_can_extract_multiple_global_pvs(self):
-        xml = """<?xml version="1.0" ?>
-                    <blocks xmlns="http://epics.isis.rl.ac.uk/schema/blocks/1.0" 
-                    xmlns:blk="http://epics.isis.rl.ac.uk/schema/blocks/1.0" xmlns:xi="http://www.w3.org/2001/XInclude">
-                        <block>
-                            <name>S2VG</name>
-                            <read_pv>MOT:JAWS2:VGAP</read_pv>
-                            <local>False</local>
-                            <visible>True</visible>
-                            <rc_enabled>False</rc_enabled>
-                            <rc_lowlimit>0.0</rc_lowlimit>
-                            <rc_highlimit>0.0</rc_highlimit>
-                            <log_periodic>True</log_periodic>
-                            <log_rate>30</log_rate>
-                            <log_deadband>0.0</log_deadband>
-                        </block>
-                        <block>
-                            <name>Cy</name>
-                            <read_pv>CY</read_pv>
-                            <local>False</local>
-                            <visible>True</visible>
-                            <rc_enabled>False</rc_enabled>
-                            <rc_lowlimit>0.0</rc_lowlimit>
-                            <rc_highlimit>0.0</rc_highlimit>
-                            <log_periodic>True</log_periodic>
-                            <log_rate>30</log_rate>
-                            <log_deadband>0.0</log_deadband>
-                        </block>
-                        <block>
-                            <name>Cx</name>
-                            <read_pv>CX</read_pv>
-                            <local>False</local>
-                            <visible>True</visible>
-                            <rc_enabled>False</rc_enabled>
-                            <rc_lowlimit>0.0</rc_lowlimit>
-                            <rc_highlimit>0.0</rc_highlimit>
-                            <log_periodic>True</log_periodic>
-                            <log_rate>30</log_rate>
-                            <log_deadband>0.0</log_deadband>
-                        </block>
-                    </blocks>"""
+        xml = generic_block_xml.format(
+                                """<block>
+                                    <name>S2VG</name>
+                                    <read_pv>MOT:JAWS2:VGAP</read_pv>
+                                    <local>False</local>
+                                    <visible>True</visible>
+                                    <rc_enabled>False</rc_enabled>
+                                    <rc_lowlimit>0.0</rc_lowlimit>
+                                    <rc_highlimit>0.0</rc_highlimit>
+                                    <log_periodic>True</log_periodic>
+                                    <log_rate>30</log_rate>
+                                    <log_deadband>0.0</log_deadband>
+                                </block>""",
+                                """<block>
+                                    <name>Cy</name>
+                                    <read_pv>CY</read_pv>
+                                    <local>False</local>
+                                    <visible>True</visible>
+                                    <rc_enabled>False</rc_enabled>
+                                    <rc_lowlimit>0.0</rc_lowlimit>
+                                    <rc_highlimit>0.0</rc_highlimit>
+                                        <log_periodic>True</log_periodic>
+                                        <log_rate>30</log_rate>
+                                        <log_deadband>0.0</log_deadband>
+                                    </block>""",
+                                """<block>
+                                    <name>Cx</name>
+                                    <read_pv>CX</read_pv>
+                                    <local>False</local>
+                                    <visible>True</visible>
+                                    <rc_enabled>False</rc_enabled>
+                                    <rc_lowlimit>0.0</rc_lowlimit>
+                                    <rc_highlimit>0.0</rc_highlimit>
+                                    <log_periodic>True</log_periodic>
+                                    <log_rate>30</log_rate>
+                                    <log_deadband>0.0</log_deadband>
+                                </block>""")
 
         self.assertListEqual(self.config_utils.get_block_pvs_from_xml("IN:DEMO", xml), ["MOT:JAWS2:VGAP", "CY", "CX"])
 
     def test_GIVEN_block_xml_WHEN_parsed_THEN_can_extract_one_local_pv(self):
-        xml = """<?xml version="1.0" ?>
-                    <blocks xmlns="http://epics.isis.rl.ac.uk/schema/blocks/1.0" 
-                    xmlns:blk="http://epics.isis.rl.ac.uk/schema/blocks/1.0" xmlns:xi="http://www.w3.org/2001/XInclude">
-                        <block>
-                            <name>S2VG</name>
-                            <read_pv>MOT:JAWS2:VGAP</read_pv>
-                            <local>True</local>
-                            <visible>True</visible>
-                            <rc_enabled>False</rc_enabled>
-                            <rc_lowlimit>0.0</rc_lowlimit>
-                            <rc_highlimit>0.0</rc_highlimit>
-                            <log_periodic>True</log_periodic>
-                            <log_rate>30</log_rate>
-                            <log_deadband>0.0</log_deadband>
-                        </block>
-                    </blocks>"""
+        xml = generic_block_xml.format(
+                                """<block>
+                                    <name>S2VG</name>
+                                    <read_pv>MOT:JAWS2:VGAP</read_pv>
+                                    <local>True</local>
+                                    <visible>True</visible>
+                                    <rc_enabled>False</rc_enabled>
+                                    <rc_lowlimit>0.0</rc_lowlimit>
+                                    <rc_highlimit>0.0</rc_highlimit>
+                                    <log_periodic>True</log_periodic>
+                                    <log_rate>30</log_rate>
+                                    <log_deadband>0.0</log_deadband>
+                                </block>""", "", "")
 
         self.assertListEqual(self.config_utils.get_block_pvs_from_xml("IN:DEMO:", xml), ["IN:DEMO:MOT:JAWS2:VGAP"])
 
@@ -196,7 +189,6 @@ class ConfigurationTests(unittest.TestCase):
         self.assertEqual(macros_2[name_2], value_2_02)
 
     def test_GIVEN_ioc_xml_WHEN_macros_requested_for_ioc_that_does_not_exist_THEN_returns_no_data(self):
-
         xml = """<?xml version="1.0" ?>
                     <iocs xmlns="http://epics.isis.rl.ac.uk/schema/iocs/1.0" 
                     xmlns:ioc="http://epics.isis.rl.ac.uk/schema/iocs/1.0" xmlns:xi="http://www.w3.org/2001/XInclude">
@@ -207,7 +199,6 @@ class ConfigurationTests(unittest.TestCase):
         self.assertEqual(len(self.config_utils.get_ioc_macros(xml, "SIMPL").values()), 0)
 
     def test_GIVEN_ioc_xml_WHEN_simlevel_is_not_none_THEN_returns_false(self):
-
         xml = """<?xml version="1.0" ?>
                     <iocs xmlns="http://epics.isis.rl.ac.uk/schema/iocs/1.0" 
                     xmlns:ioc="http://epics.isis.rl.ac.uk/schema/iocs/1.0" xmlns:xi="http://www.w3.org/2001/XInclude">
@@ -226,7 +217,6 @@ class ConfigurationTests(unittest.TestCase):
         self.assertFalse(self.config_utils.get_ioc_in_sim_mode(xml, "SIMPLE_01"))
 
     def test_GIVEN_ioc_xml_WHEN_simlevel_is_none_THEN_returns_true(self):
-
         xml = """<?xml version="1.0" ?>
                     <iocs xmlns="http://epics.isis.rl.ac.uk/schema/iocs/1.0" 
                     xmlns:ioc="http://epics.isis.rl.ac.uk/schema/iocs/1.0" xmlns:xi="http://www.w3.org/2001/XInclude">
