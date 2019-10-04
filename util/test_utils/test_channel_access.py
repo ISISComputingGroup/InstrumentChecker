ef73e3520aa16e0dd154b3662188ee46e7765545
Alexandru Dascalu <965337@swansea.ac.uk>
2019-10-04 13:47:49 +0100

    renamed test PVs

--- util/test_utils/test_channel_access.py
+++ util/test_utils/test_channel_access.py
@@ -8,15 +8,15 @@ class ChannelAccessTests(unittest.TestCase):
     def setUp(self):
         def mock_get_value(pv):
             if pv == 'CS:BLOCKSERVER:PVS:INTEREST:HIGH':
-                return """[["IN:DEMO:WM323_02:SPEED", "longin", "Current pump speed", "WM323_02"],\
-                ["IN:DEMO:WM323_02:STATUS", "bi", "Pump running status", "WM323_02"]]"""
+                return """[["IN:DEMO:PV_HIGH_INTEREST_1", "longin", "Current pump speed", "WM323_02"],\
+                ["IN:DEMO:PV_HIGH_INTEREST_2", "bi", "Pump running status", "WM323_02"]]"""
             elif pv == 'CS:BLOCKSERVER:PVS:INTEREST:MEDIUM':
-                return """[["IN:DEMO:ALDN1000_01:PROGRAM:FUNCTION", "mbbi", "Pumping Program Phase's function",\
-                "ALDN1000_01"], ["IN:DEMO:ALDN1000_01:VOLUME:INF:CLEAR:SP", "bo",\
-                 "Set infused volume dispensed to 0.0", "ALDN1000_01"]]"""
+                return """[["IN:DEMO:PV_MEDIUM_INTEREST_1", "mbbi", "Pumping Program Phase's function",\
+                "ALDN1000_01"], ["IN:DEMO:PV_MEDIUM_INTEREST_2", "bo",\
+                "Set infused volume dispensed to 0.0", "ALDN1000_01"]]"""
             elif pv == 'CS:BLOCKSERVER:PVS:INTEREST:LOW':
-                return """[["IN:DEMO:ALDN1000_01:VOLUME:WDR:CLEAR:SP", "bo", "Set withdrawn volume dispensed to 0.0",\
-                "ALDN1000_01"], ["IN:DEMO:AMINT2L_01:RANGE:ERROR", "mbbi", "Error in pressure readback",\
+                return """[["IN:DEMO:PV_LOW_INTEREST_1", "bo", "Set withdrawn volume dispensed to 0.0",\
+                "ALDN1000_01"], ["IN:DEMO:PV_LOW_INTEREST_2", "mbbi", "Error in pressure readback",\
                 "AMINT2L_01"]]"""
             else:
                 raise ValueError('pv argument needs to be HIGH, MEDIUM or LOW instrument PV for list of pvs by '
@@ -38,12 +38,12 @@ class ChannelAccessTests(unittest.TestCase):
 
     def test_GIVEN_interesting_level_WHEN_high_THEN_correct_high_pvs_returned(self):
         self.assertListEqual(self.channel_access._get_pvs_by_interesting_level(PvInterestingLevel.HIGH),
-                             ["IN:DEMO:WM323_02:SPEED", "IN:DEMO:WM323_02:STATUS"])
+                             ["IN:DEMO:PV_HIGH_INTEREST_1", "IN:DEMO:PV_HIGH_INTEREST_2"])
 
     def test_GIVEN_interesting_level_WHEN_medium_THEN_correct_medium_pvs_returned(self):
         self.assertListEqual(self.channel_access._get_pvs_by_interesting_level(PvInterestingLevel.MEDIUM),
-                             ["IN:DEMO:ALDN1000_01:PROGRAM:FUNCTION", "IN:DEMO:ALDN1000_01:VOLUME:INF:CLEAR:SP"])
+                             ["IN:DEMO:PV_MEDIUM_INTEREST_1", "IN:DEMO:PV_MEDIUM_INTEREST_2"])
 
     def test_GIVEN_interesting_level_WHEN_low_THEN_correct_low_pvs_returned(self):
         self.assertListEqual(self.channel_access._get_pvs_by_interesting_level(PvInterestingLevel.LOW),
-                             ["IN:DEMO:ALDN1000_01:VOLUME:WDR:CLEAR:SP", "IN:DEMO:AMINT2L_01:RANGE:ERROR"])
+                             ["IN:DEMO:PV_LOW_INTEREST_1", "IN:DEMO:PV_LOW_INTEREST_2"])
