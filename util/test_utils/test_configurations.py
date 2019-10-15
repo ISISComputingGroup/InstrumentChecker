8b3d9794715899fcbc0b8dfaa047f257b72a9557
Alexandru Dascalu <965337@swansea.ac.uk>
2019-10-15 14:59:30 +0100

    added unit test to check static get pv name without field method

--- util/test_utils/test_configurations.py
+++ util/test_utils/test_configurations.py
@@ -1,4 +1,4 @@
-from util.configurations import ConfigurationUtils
+from util.configurations import ConfigurationUtils, AbstractConfigurationUtils
 import unittest
 
 generic_component_xml = """<?xml version="1.0" ?>
@@ -113,6 +113,14 @@ class ConfigurationTests(unittest.TestCase):
                                 </block>""", "", "")
 
         self.assertListEqual(self.config_utils.get_block_pvs_from_xml("IN:DEMO:", xml), ["IN:DEMO:MOT:JAWS2:VGAP"])
+
+    def test_GIVEN_pv_name_THEN_returns_it_unchanged(self):
+        self.assertEqual(AbstractConfigurationUtils._get_pv_name_without_field('MOT:JAWS2:MTR0123'),
+                         'MOT:JAWS2:MTR0123')
+
+    def test_GIVEN_field_name_THEN_returns_pv_name(self):
+        self.assertEqual(AbstractConfigurationUtils._get_pv_name_without_field('MOT:JAWS2:MTR0123.RBV'),
+                         'MOT:JAWS2:MTR0123')
 
     def test_GIVEN_block_xml_WHEN_parsed_THEN_can_extract_one_local_pv(self):
         xml = generic_block_xml.format(
