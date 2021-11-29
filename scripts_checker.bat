748e51e435774b17b168182ff8fa62d457ab98a2
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2021-11-29 19:00:04 +0000

    Update scripts_checker.bat

--- scripts_checker.bat
+++ scripts_checker.bat
@@ -4,5 +4,5 @@ REM Create local python environment from genie python on share
 git clone https://github.com/ISISComputingGroup/ibex_utils.git
 CALL ibex_utils\installation_and_upgrade\define_latest_genie_python.bat
 
-%LATEST_PYTHON% scripts_checker.py
+%LATEST_PYTHON% -u scripts_checker.py
 if %errorlevel% neq 0 exit /b %errorlevel%
