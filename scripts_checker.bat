c0c39be38115c573dd246389f6917b602cfc8be6
Jack Doughty <jackb.doughty@btinternet.com>
2024-09-02 11:19:12 +0100

    Update scripts_checker to install/remove python

--- scripts_checker.bat
+++ scripts_checker.bat
@@ -1,8 +1,15 @@
 setlocal
 
 REM Create local python environment from genie python on share
-git clone https://github.com/ISISComputingGroup/ibex_utils.git
+rem git clone https://github.com/ISISComputingGroup/ibex_utils.git
 CALL ibex_utils\installation_and_upgrade\define_latest_genie_python.bat
 
 %LATEST_PYTHON% -u scripts_checker.py
-if %errorlevel% neq 0 exit /b %errorlevel%
+
+if %errorlevel% neq 0 (
+    set errcode = %ERRORLEVEL%
+    call ibex_utils\installation_and_upgrade\remove_genie_python.bat %LATEST_PYTHON_DIR%
+    EXIT /b !errcode!
+)
+
+call ibex_utils\installation_and_upgrade\remove_genie_python.bat %LATEST_PYTHON_DIR%
