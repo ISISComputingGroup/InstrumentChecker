2dab8b21be646d589399e46dc62e2b82b7b561c5
George Ryall <George.Ryall@stfc.ac.uk>
2025-06-09 17:04:19 +0100

    ruff

--- tests/dae_tests.py
+++ tests/dae_tests.py
@@ -27,5 +27,5 @@ class DaeTests(unittest.TestCase):
             self.assertGreater(
                 failure_threshold_percent / 100 * (10 ** len(current_run_number)),
                 int(current_run_number),
-                f"The current run number is within {failure_threshold_percent}% ({100*int(current_run_number)/(10**len(current_run_number)):.1f}%)of the maximum run number",
+                f"The current run number is within {failure_threshold_percent}% ({100 * int(current_run_number) / (10 ** len(current_run_number)):.1f}%)of the maximum run number",
             )
