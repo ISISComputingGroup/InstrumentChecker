9ceb08201af1af17529180cfc5c90b4ac4f3126c
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2025-06-16 11:14:03 +0100

    Ticket 5220

--- tests/motor_tests.py
+++ tests/motor_tests.py
@@ -45,3 +45,43 @@ class MotorTests(unittest.TestCase):
                 result = fut.result()
                 with self.subTest():
                     self.assertIsNone(result)
+
+    def test_galils_have_nonzero_encoder_sync_tolerance(self) -> None:
+        """
+        Closed-loop galil axes must have a non-zero motor-encoder sync tolerance set.
+
+        This only applies to closed-loop axes; it is not meaningful for open-loop axes.
+
+        Generally an appropriate _MOT_ENC_SYNC_TOL_SP is ~ERES, this will cause motor and encoder
+        to resync before a move if they differ by more than one encoder step.
+
+        This test ensures that ._MOT_ENC_SYNC_TOL_SP is not set to zero
+        for any closed-loop galil axes.
+        """
+
+        def check_nonzero_mot_enc_sync_tol(controller, motor) -> str | None:
+            prefix = f"MOT:MTR{controller:02d}{motor:02d}"
+            controller_type = self.ca.get_value(f"{prefix}_IOCNAME", timeout=30)
+
+            if controller_type is not None and controller_type.startswith("GALIL_"):
+                mot_enc_sync_tol = self.ca.get_value(f"{prefix}_MOT_ENC_SYNC_TOL_SP", timeout=30)
+                ueip = self.ca.get_value(f"{prefix}.UEIP", timeout=30)
+                if ueip and mot_enc_sync_tol == 0:
+                    return f"Motor-encoder sync tolerance is zero on closed-loop axis {self.ca.pv_prefix}{prefix}"
+
+            return None
+
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=MAX_MOTOR * MAX_CONTROLLER
+        ) as executor:
+            futures = []
+            for controller in range(1, MAX_CONTROLLER + 1):
+                for motor in range(1, MAX_MOTOR + 1):
+                    futures.append(
+                        executor.submit(check_nonzero_mot_enc_sync_tol, controller, motor)
+                    )
+
+            for fut in concurrent.futures.as_completed(futures):
+                result = fut.result()
+                with self.subTest():
+                    self.assertIsNone(result)
